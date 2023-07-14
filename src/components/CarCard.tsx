d638d7fbc4b832754270e188abfeb72d4600bf95
Rentsen <rentsena1@gmail.com>
2023-07-13 23:18:32 -0800

    aa

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -54,7 +54,7 @@ const CarCard = ({car}:CarCardProps) => {
 </div>
 <div className="car-card__btn-container">
           <CustomButton
-            title='View More'
+            title='Дэлгэрэнгүй'
             containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
             textStyles='text-white text-[14px] leading-[17px] font-bold'
             rightIcon='/right-arrow.svg'
